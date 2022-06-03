d2dbd4329f8f653a827294965423e8d5ba40f9fe
imbesat-rizvi <imbugene@gmail.com>
2022-06-03 08:39:40 +0530

    regex bug fix preventing multiple whitespace being added in preprocess_text

--- utils.py
+++ utils.py
@@ -5,6 +5,6 @@ from string import punctuation
 def preprocess_text(text):
     # replace numbers and punctuations with whitespace
     # and convert to lower case
-    pattern = f"[{punctuation}0-9]"
+    pattern = f"[{punctuation}0-9]+"
     text = re.sub(pattern, " ", text.lower())
     return text
